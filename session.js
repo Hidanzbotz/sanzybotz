095610aa36e4be7e0d0ccf8d6c48c2b11254e394
Hidanzbotz <91834504+Hidanzbotz@users.noreply.github.com>
2021-12-31 02:21:46 +0800

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+        "clientID": "LKHf9wI/rjDqiUIos5rM1A==",
+        "serverToken": "1@cIFwQXLrG8XKMJtnZOdtDu63Eyv7chdvXbYNpkad99KR1iB0j1NhhbPXjmosaLwprd5HmMyVXPedCA==",
+        "clientToken": "UEBEHQ45d4lyvqW3ajoVxfuMsmzONaJlrmk3Ka1lfPY=",
+        "encKey": "ljkKepYA13CFjVXVs2MOdcTftX1bwJWNrejsTlJhHec=",
+        "macKey": "LUEUBuPsWHPXKxwCSmL1hraaTGhQ3AzSRlhKjxwhyUo="
+}
